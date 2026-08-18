190f2ffd02e2b0619764802312be8014b74e51e4
colchinvlad-dev <colchin.vlad@gmail.com>
2026-08-18 10:54:23 +0300

    --fix-2

--- src/widgets/skill-section/skill-section.tsx
+++ src/widgets/skill-section/skill-section.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/shared/ui/button'
 import { Icon } from '@/shared/ui/icon'
-import { SkillCard, type SkillCardProps } from '@/entities/user/ui/skill-card' 
+import { SkillCard, type SkillCardProps } from '@/widgets/skill-card' 
 import styles from './skill-section.module.css'
 
 export interface SkillSectionProps {
