763935cf7c1ded2050ee9d65fdef98c551010fbe
Vladimir <vlapov@gmail.com>
2026-08-23 18:37:44 +0300

    feat: добавить NotificationsMenu и подключить в AuthHeader

--- src/widgets/notifications-menu/index.ts
+++ src/widgets/notifications-menu/index.ts
@@ -3,7 +3,3 @@ export type {
   NotificationsMenuProps,
   NotificationItem,
 } from './notifications-menu'
-export {
-  DEMO_NEW_NOTIFICATIONS,
-  DEMO_VIEWED_NOTIFICATIONS,
-} from './notifications-menu.demo'
